2c12dd122aa95c24d162087b15c7cb7a412d06cd
Ahmed-878 <ahmedabouelhaggag06@gmail.com>
2025-05-07 19:14:59 +0300

    Change build to match host-mfe

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
